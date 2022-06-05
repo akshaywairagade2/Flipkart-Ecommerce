cb3020a91c52768300b55d3c07b5801f1e964645
Akshay <akshaywairagade2@gmail.com>
2022-06-05 18:11:01 +0530

    check

--- flipkart-clone/src/helpers/axios.js
+++ flipkart-clone/src/helpers/axios.js
@@ -3,6 +3,7 @@ import { api } from '../urlConfig';
 
 const token=window.localStorage.getItem('token');
 // console.log(token);
+// console.log(token);
 const axiosInstance=axios.create({
     baseURL: api,
     headers:{

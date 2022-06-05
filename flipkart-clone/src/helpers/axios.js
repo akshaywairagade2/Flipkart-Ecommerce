3867eb6263972d6bd7e800c8aac5ab0027faa79d
Akshay <akshaywairagade2@gmail.com>
2022-06-05 18:20:50 +0530

    time

--- flipkart-clone/src/helpers/axios.js
+++ flipkart-clone/src/helpers/axios.js
@@ -4,7 +4,6 @@ import { api } from '../urlConfig';
 const token=window.localStorage.getItem('token');
 // console.log(token);
 // console.log(token);
-// console.log(token);
 const axiosInstance=axios.create({
     baseURL: api,
     headers:{
